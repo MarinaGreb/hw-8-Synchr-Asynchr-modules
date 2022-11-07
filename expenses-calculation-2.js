89baab58d652f88514e89b4434d4bc96b5b1c22f
marinagrebneva <sokovets@outlook.com>
2022-11-07 09:46:50 +0100

    fixed for two files

--- expenses-calculation-2.js
+++ expenses-calculation-2.js
@@ -4,12 +4,14 @@ let expensesArray = [
   { expenses: [20, 200, 8900, 3300, 2300, 1009, 1200, 2000, 670, 900, 900, 7000]},
 ];
 
-let sumArr = [];//массив с суммами затрат по всем объектам
-let sumAllExpenses = 0;//сумма затрат по всем массивам- во всех объектах
+let sumArr = []; //массив с суммами затрат по всем объектам
+let sumAllExpenses = 0; //сумма затрат по всем массивам- во всех объектах
 
-function calculateSum(arr) {//функция для расчёта суммы затрат в одном объекте, значение которого представлено массивом
-    let sumExpense = 0;// сумма затрат в одном объекте 
-    arr.forEach((e) => {//для всех элементов в массиве
+function calculateSum(arr) {
+  //функция для расчёта суммы затрат в одном объекте, значение которого представлено массивом
+  let sumExpense = 0; // сумма затрат в одном объекте
+  arr.forEach((e) => {
+    //для всех элементов в массиве
     if (e > 1000) {
       sumExpense += e;
     }
@@ -17,41 +19,22 @@ function calculateSum(arr) {//функция для расчёта суммы з
   return sumExpense;
 }
 
-
-for (let i=0; i < expensesArray.length; i++){
-  console.log(`Сумма затрат для ${i}го массива: ${calculateSum(expensesArray[i].expenses)}`)
+for (let i = 0; i < expensesArray.length; i++) {
+  let sumExpense = calculateSum(expensesArray[i].expenses);
+  console.log(`Сумма затрат для ${i}го массива: ${sumExpense}`);
+  sumAllExpenses += sumExpense;
 }
 
-
-//calculateAmount(arr1);
-/*
-expensesArray.forEach((e) => { //для всех объектов-"e" в массиве "expensesArray"
-  let expensesArrItem = e.expenses; //массив для передачи значения - массива, из объекта "e" по ключу expenses
-  let arrItemSum = calculateSum(expensesArrItem);//переменная куда возвращено значение "sumExpense"-сумма по одному массиву
-  sumArr.push(arrItemSum);
-  sumAllExpenses += arrItemSum;
-});*/
-
-
-
-console.log(`Сумма затрат по каждому объекту в массиве: ${sumArr}`);
 console.log(`Расчет суммы затрат по всем объектам: ${sumAllExpenses}`)
-
-
-
-
-
-
-
 
 //TESTS
 
 function testCalculateSum(arr, expectedResult){
-    return calculateSum(arr) == expectedResult;
+  return calculateSum(arr) == expectedResult;
 }
 
 let expectedResults = [40590, 148200, 25709];
 for (let i=0; i < expectedResults.length; i++){
-    console.log(`Результат прохождения теста для ${i}-го массива`);
-    console.log(testCalculateSum(expensesArray[i].expenses, expectedResults[i]))
+  console.log(`Результат прохождения теста для ${i}-го массива:`);
+  console.log(testCalculateSum(expensesArray[i].expenses, expectedResults[i]))
 }
--- monthly-expenses.js
+++ monthly-expenses.js
@@ -5,49 +5,21 @@ let expensesArray = [
 ];
 
 
-let arrayOfMonths = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Ноябрь', 'Декабрь'];
+const arrayOfMonths = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 
+                      'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
 
-
-function monthlyExpenses(arr, mounth) {
+function monthlyExpenses(arr) {
     arr.forEach((e, index) => {//для всех элементов в массиве
     if (e <= 1000) {
-    //  new Date(2022, index, 01);
-      console.log(mounth[index])
+    let month = new Date(2022, index).getMonth();
+    //console.log(month.getMonth())
+    //console.log(arrayOfMonths[index])//я бы лучше использовала данный вариант, особо не вижу смысла в использовании Date(
+    console.log(arrayOfMonths[month])
     }
   });
-
-
-
-  //console.log(sumExpense);
-  return sumExpense;
 }
 
-for (let i=0; i < expectedResults.length; i++){
-  console.log(monthlyExpenses(expensesArray[i].expenses, arrayOfMonths))
+for (let i=0; i < expensesArray.length; i++){
+  console.log(`Месяцы для ${i+1}-го массива затрат:`)
+  monthlyExpenses(expensesArray[i].expenses);
 }
-
-
-expensesArray.forEach((e) => { //для всех объектов-"e" в массиве "expensesArray"
-  let expensesArrItem = e.expenses; //массив для передачи значения - массива, из объекта "e" по ключу expenses
-  let arrItemSum = calculateSum(expensesArrItem);//переменная куда возвращено значение "sumExpense"-сумма по одному массиву
-  sumArr.push(arrItemSum);
-  sumAllExpenses += arrItemSum;
-});
-
-
-console.log(`Сумма затрат по каждому объекту в массиве: ${sumArr}`);
-console.log(`Расчет суммы затрат по всем объектам: ${sumAllExpenses}`)
-
-//TESTS
-
-function testCalculateSum(arr, expectedResult){
-    return calculateSum(arr) == expectedResult;
-}
-
-//console.log(testCalculateSum(expensesArray[0].expenses, 40590))
-
-let expectedResults = [40590, 148200, 25709];
-for (let i=0; i < expectedResults.length; i++){
-    console.log(testCalculateSum(expensesArray[i].expenses, expectedResults[i]))
-}
-
